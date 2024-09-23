020656cfa07f534bc3d194604fe3d9f8fac6e2c2
AliciaEA <aliestrada@uamv.edu.ni>
2024-09-23 08:22:13 -0600

    Creacion de la clase Registro

---
+++ Corte1/Corte1/models/Registro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1.models
+{
+    internal class Registro
+    {
+        Persona[] personas = new Persona[30];
+
+        public void AgregarPersona(Persona persona, int posicion)
+        {
+            personas[posicion] = persona;
+        }
+
+        public Persona[] GetPersonas()
+        {
+            return personas;
+        }
+
+        
+    }
+}
